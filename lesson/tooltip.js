8b624775ecd6068b873c3202efa43594fe96c81c
ASUS <18846433038@163.co'm>
2017-07-20 10:05:38 +0800

    曹宇练习

---
+++ lesson/tooltip.js
@@ -0,0 +1,33 @@
+/**
+ * Created by ASUS on 2017/7/20.
+ */
+/**
+ * Created by caoyu on 2017/7/19.
+ */
+function tooltip(selector){
+
+    $(selector).hover(function(e){
+        this.tip = $(this).attr("title")?$(this).attr("title"):$(this).html();
+        if($(".tip").length == 0){
+            var $tooltip = $("<div class='tip'></div>").html(this.tip);
+            $tooltip.appendTo($("body"));
+        }else{
+            $(".tip").html(this.tip);
+        }
+
+        $(".tip").offset({
+            top: e.pageY + 10,
+            left: e.pageX + 10
+        });
+
+        $(this).removeAttr("title");
+    }, function(){
+        $(this).attr("title", this.tip);
+        $(".tip").remove();
+    }).on("mousemove", function(e){
+        $(".tip").offset({
+            top: e.pageY + 10,
+            left: e.pageX + 10
+        });
+    });
+}
